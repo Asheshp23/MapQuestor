c0b47d96381ed2130ee0d050f99bb989c72df3eb
ApiOSDev <attup23@gmail.com>
2024-10-25 19:38:38 -0400

    Initial Commit

---
+++ MapQuestor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MapQuestor
+//
+//  Created by Ashesh Patel on 2024-10-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapQuestor/MapQuestorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapQuestorApp.swift
+//  MapQuestor
+//
+//  Created by Ashesh Patel on 2024-10-25.
+//
+
+import SwiftUI
+
+@main
+struct MapQuestorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
